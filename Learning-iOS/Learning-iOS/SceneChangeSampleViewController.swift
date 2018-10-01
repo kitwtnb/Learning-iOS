ba0b9e4b03452255c6a7e0bb5faf657b20916368
Keita Watanabe <Nal3eCh@gmail.com>
2018-10-01 23:10:15 +0900

    add scene change of some types

---
+++ Learning-iOS/Learning-iOS/SceneChangeSampleViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SceneChangeSampleViewController.swift
+//  Learning-iOS
+//
+//  Created by Keita Watanabe on 2018/10/01.
+//  Copyright © 2018 Keita Watanabe. All rights reserved.
+//
+
+import UIKit
+
+class SceneChangeSampleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func unwindToTop(seque: UIStoryboardSegue) {
+        // Nothing
+    }
+}
