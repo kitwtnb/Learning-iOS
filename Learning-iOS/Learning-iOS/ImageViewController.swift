2d106e682beb32da20762739a906681915c62d86
Keita Watanabe <Nal3eCh@gmail.com>
2018-10-15 08:11:12 +0900

    add showing image

---
+++ Learning-iOS/Learning-iOS/ImageViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ImageViewController.swift
+//  Learning-iOS
+//
+//  Created by Keita Watanabe on 2018/10/15.
+//  Copyright © 2018 Keita Watanabe. All rights reserved.
+//
+
+import UIKit
+import Photos
+
+class ImageViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var asset: PHAsset? = nil
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let asset = asset else { return }
+        let manager = PHImageManager()
+        manager.requestImage(for: asset,
+                             targetSize: view.frame.size,
+                             contentMode: .aspectFit,
+                             options: nil,
+                             resultHandler: { [weak self] (image, info) in
+                                guard let self = self, let image = image else { return }
+                                self.imageView.image = image
+        })
+    }
+}
