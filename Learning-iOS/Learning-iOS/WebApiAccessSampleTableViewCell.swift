618e03005ab8c473a44366b6621de96d96365dfe
Keita Watanabe <Nal3eCh@gmail.com>
2018-10-16 14:19:07 +0900

    add view controller and cell

---
+++ Learning-iOS/Learning-iOS/WebApiAccessSampleTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  WebApiAccessSampleTableViewCell.swift
+//  Learning-iOS
+//
+//  Created by Keita Watanabe on 2018/10/16.
+//  Copyright © 2018 Keita Watanabe. All rights reserved.
+//
+
+import UIKit
+
+class WebApiAccessSampleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
