a5960eefc3882ffad3f9a0460b215337a17f1122
Keita Watanabe <Nal3eCh@gmail.com>
2019-05-07 18:48:51 +0900

    change table view

---
+++ Learning-iOS/MainPresenter.swift
@@ -0,0 +1,15 @@
+//
+// Created by Keita Watanabe on 2019-05-07.
+// Copyright (c) 2019 Keita Watanabe. All rights reserved.
+//
+
+class MainPresenter {
+    let segues = [
+        "Scene change",
+        "Counter",
+        "Navigation with parameter",
+        "Camera",
+        "Camera roll",
+        "Web API access"
+    ]
+}
--- Learning-iOS/MainViewController.swift
+++ Learning-iOS/MainViewController.swift
@@ -9,18 +9,33 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    @IBOutlet private weak var tableView: UITableView!
+
+    private let presenter = MainPresenter()
     private let database: Database<ContributorObject> = Dependency.resolveDatabase()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+}
 
-    @IBAction func unwindToTop(seque: UIStoryboardSegue) {
-        // Nothing
+extension MainViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return presenter.segues.count
     }
     
-    @IBAction func tapClearRealmData(_ sender: Any) {
-        _ = database.deleteAll().subscribe()
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = presenter.segues[indexPath.row]
+        
+        return cell
+    }
+}
+
+extension MainViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        performSegue(withIdentifier: presenter.segues[indexPath.row], sender: nil)
     }
 }
