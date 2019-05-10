7b3f5d7c3403501dcb91bed7fddcd571455218ad
Keita Watanabe <Nal3eCh@gmail.com>
2019-05-10 15:13:52 +0900

    implement realm sample

--- Learning-iOS/view/RealmSample/RealmSampleViewController.swift
+++ Learning-iOS/view/RealmSample/RealmSampleViewController.swift
@@ -6,35 +6,63 @@
 //  Copyright © 2019 Keita Watanabe. All rights reserved.
 //
 
+import RealmSwift
 import UIKit
 
+class Record: Object {
+    @objc dynamic var title = ""
+}
+
 class RealmSampleViewController: UIViewController {
+    @IBOutlet private weak var tableView: UITableView!
+    @IBOutlet private weak var inputText: UITextField!
+
+    private var records: [Record] = []
+    private var notificationToken: NotificationToken? = nil
+
+    deinit {
+        notificationToken?.invalidate()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let realm = try! Realm()
+        let results = realm.objects(Record.self)
+        notificationToken = results.observe { [weak self] changes in
+            switch changes {
+            case .initial(results): fallthrough
+            case .update(results, _, _, _):
+                self?.records = Array(results)
+                self?.tableView.reloadData()
+            default: break
+            }
+        }
     }
-    
 
-    /*
-    // MARK: - Navigation
+    @IBAction func onTappedSave(_ sender: Any) {
+        guard let text = inputText.text, !text.isEmpty else { return }
+        inputText.text = ""
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let record = Record()
+        record.title = text
+
+        let realm = try! Realm()
+        try! realm.write {
+            realm.add(record)
+        }
     }
-    */
-
 }
 
 extension RealmSampleViewController: UITableViewDataSource {
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return records.count
     }
 
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = records[indexPath.row].title
+
+        return cell
     }
 }
